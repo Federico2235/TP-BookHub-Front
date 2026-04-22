3c2fad1054e4a9ed0433ab36a2ec00f017ebd378
ChrisVai <chrisbellet@gmail.com>
2026-04-22 16:27:52 +0200

    bugFix + un petit peu de css quand même on va pas se le cacher

--- src/app/features/books/pages/home/home.ts
+++ src/app/features/books/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { BooksService } from '../../services/books-service';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AvailabilityStatus } from '../../models/availabilityStatus.model';
 
 @Component({
   selector: 'app-home',
@@ -13,4 +14,5 @@ export class Home {
   private bookService = inject(BooksService);
 
   _books = this.bookService.getBooks();
+  protected readonly AvailabilityStatus = AvailabilityStatus;
 }
