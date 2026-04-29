16709c47223f93a09dba6a046324704fd63d5d46
ChrisVai <144102898+ChrisVai@users.noreply.github.com>
2026-04-29 16:13:22 +0200

    filtres dashboard admin (#42)

--- src/app/features/users/pages/librarian-board/librarian-board.ts
+++ src/app/features/users/pages/librarian-board/librarian-board.ts
@@ -9,33 +9,60 @@ import { BookCondition } from '../../../books/models/bookCondition.model';
 import { AvailabilityStatus } from '../../../books/models/availabilityStatus.model';
 import { Reservation } from '../../../../shared/model/Reservation.model';
 import { BorrowCreate } from '../../../../shared/model/BorrowCreate.model';
+import { form, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-librarian-board',
-  imports: [DatePipe],
+  imports: [DatePipe, FormField],
   templateUrl: './librarian-board.html',
   styleUrl: './librarian-board.css',
 })
 export class LibrarianBoard {
+  //Dependencies
   private userService: UserService = inject(UserService);
   private reservationService: ReservationService = inject(ReservationService);
   private bookService: BooksService = inject(BooksService);
   private borrowService: BorrowService = inject(BorrowService);
 
+  //Enums
   protected readonly BookCondition = BookCondition;
   protected readonly AvailabilityStatus = AvailabilityStatus;
 
+  //Forms
+  searchReservationInput = signal<string>('');
+  searchBorrowsInput = signal<string>('');
+  searchBookInput = signal<string>('');
+  searchReservationForm = form<string>(this.searchReservationInput);
+  searchBorrowsForm = form<string>(this.searchBorrowsInput);
+  searchBookForm = form<string>(this.searchBookInput);
+
+  //Data as Signals
   user: WritableSignal<User | null> = this.userService.loggedUserSignal;
   books = this.bookService.allBooks.asReadonly();
   borrows = this.borrowService.allBorrows.asReadonly();
   reservations = this.reservationService.allReservations.asReadonly();
-  currentBorrows = computed(() => this.borrows().filter((borrow) => borrow.returnDate === null).reverse());
+
+  //Computed values as Signals
+  currentBorrows = computed(() =>
+    this.borrows()
+      .filter((borrow) => borrow.returnDate === null)
+  );
   overdueLoans = computed(() =>
     this.borrows().filter((borrow) => borrow.returnDate === null && this.isLate(borrow.borrowEnd)),
   );
+  reservationsFiltered = computed(() =>
+    this.reservations().filter(resa => resa.book.title.toLowerCase().includes(this.searchReservationForm().value().toLowerCase()))
+  );
+  currentBorrowsFiltered = computed(() =>
+    this.currentBorrows().filter(borrow => borrow.book.title.toLowerCase().includes(this.searchBorrowsForm().value().toLowerCase()))
+  );
+  booksFiltered = computed(() =>
+    this.books().filter(book => book.title.toLowerCase().includes(this.searchBookForm().value().toLowerCase()))
+  )
+
 
   constructor() {
-    this.updateAll()
+    this.updateAll();
   }
 
   isLate(date: Date) {
