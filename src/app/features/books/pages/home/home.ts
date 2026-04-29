a0105b0bbabdbeb8c65957760671416e405bd191
ChrisVai <144102898+ChrisVai@users.noreply.github.com>
2026-04-29 13:56:24 +0200

    Feat/filtre catalogue (#39)
    * filtre page catalogue + message de pas de résultats
    
    * Filtre par livres réservables

--- src/app/features/books/pages/home/home.ts
+++ src/app/features/books/pages/home/home.ts
@@ -1,20 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { BooksService } from '../../services/books-service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AvailabilityStatus } from '../../models/availabilityStatus.model';
+import { form, FormField } from '@angular/forms/signals';
+import { NoResultFound } from '../no-result-found/no-result-found';
 
 @Component({
   selector: 'app-home',
-  imports: [AsyncPipe, RouterLink],
+  imports: [RouterLink, FormField, NoResultFound],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   private bookService = inject(BooksService);
-  constructor() {
-    console.log('Home created');
-  }
-  _books = this.bookService.getBooks();
+  books = this.bookService.allBooks;
+  searchBarInput = signal<string>('');
+  availabilityCheckBox = signal<boolean>(false);
+  availabilityForm = form<boolean>(this.availabilityCheckBox);
+  searchBarForm = form(this.searchBarInput);
+  availableFilteredBooks = computed(() => this.books().filter((book) => !book.reserved));
+  filteredBooks = computed(() => {
+    if (this.availabilityForm().value()) {
+      return this.availableFilteredBooks().filter((book) =>
+        book.title.toLowerCase().includes(this.searchBarForm().value().toLowerCase()),
+      );
+    } else {
+      return this.books().filter((book) =>
+        book.title.toLowerCase().includes(this.searchBarForm().value().toLowerCase()),
+      );
+    }
+  });
   protected readonly AvailabilityStatus = AvailabilityStatus;
+
+  constructor() {
+    this.bookService.updateBooks()
+  }
 }
---
+++ src/app/features/books/pages/no-result-found/no-result-found.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-result-found',
+  imports: [],
+  templateUrl: './no-result-found.html',
+  styleUrl: './no-result-found.css',
+})
+export class NoResultFound {}
